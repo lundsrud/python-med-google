6ec41bdb5f4006d017f5e3e1a87299299863bdd7
lundsrud <68284776+lundsrud@users.noreply.github.com>
2023-06-20 20:22:58 +0200

    Create Defining Functions.py

---
+++ Crash Course on Python/Defining Functions.py
@@ -0,0 +1,8 @@
+>>> def greeting(name, department):
+    print("Welcome, " + name)
+    print("You are part og " + department)
+    
+>>> greeting("Oliver", "IT Support")
+
+
+>>> greeting("Martin", "DevOps")
